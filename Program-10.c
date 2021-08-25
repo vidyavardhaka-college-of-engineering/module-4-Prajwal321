e274ebde1471369b7e6981db3750596baff21f4b
Prajwal321 <84697263+Prajwal321@users.noreply.github.com>
2021-08-25 21:48:11 +0530

    Update Program-10.c

--- Program-10.c
+++ Program-10.c
@@ -1,15 +1,10 @@
-//To store n elements in an array and print the elements using pointer.
-// Input : 5 4 6 1 7 3       
-// Output: 4 6 1 7 3                                                    
-                                                                                                                                                   
-                                                                                  
 #include <stdio.h>
 int main()
 {
    int arr1[25], i,n;
    printf("Input the number of elements to store in the array :");
    scanf("%d",&n);
-   printf("Input elements of the array :\n",n);
+   printf("Input elements of the array :\n");
    for(i=0;i<n;i++)
       {
 	   scanf(" %d",&arr1[i]);
@@ -17,6 +12,7 @@ int main()
    printf("The elements you entered are : \n");
    for(i=0;i<n;i++)
       {
+        printf("%d ",arr1[i]);
 	 //print the array elements
 	  }
 	   return 0;
